bbce9e06239dbf48fcee2b08081a0ef2cb97f5e2
Filipe Freire <filipe.r.freire@gmail.com>
2016-11-22 09:09:40 +0000

    Added more changes to index and to tile.js, first experiments

--- src/js/tile.js
+++ src/js/tile.js
@@ -1,5 +1,13 @@
+var els = document.querySelectorAll('.card');
+
 (function(){
     console.log("tileMe...");
     /*
     */
+    console.log("Elements found: ", els.length);
+    for (var el in els) {
+        console.log(els[el]);
+        console.log(els[el].getBoundingClientRect());
+    }
+
 })();
