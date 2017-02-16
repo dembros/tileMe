ae742c728ab0ae2eeb9696e19d6f526de7924ae0
Filipe Freire <filipe.r.freire@gmail.com>
2017-02-16 01:48:41 +0000

    Finished first working version. Currently only working on Firefox.

--- src/js/tile.js
+++ src/js/tile.js
@@ -1,60 +1,124 @@
-(function () {
+// Create by filipe freire 2016-2017
+// Dem bros - magik
+
+
+// FIXME: bug, currently only works in Firefox - Chrome has limitations in boundyRect part
+(function() {
+
     var selector = '.card';
+    var ARROW_UP = 38;
+    var ARROW_DOWN = 40;
+    var ARROW_RIGHT = 39;
+    var ARROW_LEFT = 37;
+    var currentRow = 0;
+    var currentColumn = 0;
+    var tiles = [];
+    var maxRowLength = 3;
+    var maxColumnLength = 4;
 
     function nodeListToArray(nodeList) {
+
         var array = [];
+
         for (var i = 0; i < nodeList.length; ++i) {
             array.push(nodeList.item(i));
         }
+
         return array;
     }
 
     function getUniqueCoordinates(array, axis) {
-        var result = [];
-        array.forEach(function (el) {
+
+        var coordinatesArr = [];
+        array.forEach(function(el) {
             var axisPos = el.boundingRect[axis];
-            if (result.indexOf(axisPos) < 0) {
-                result.push(axisPos)
+            if (coordinatesArr.indexOf(axisPos) < 0) {
+                coordinatesArr.push(axisPos)
             }
         });
-        return result;
+
+        return coordinatesArr;
     }
 
-    function getOrganizedTiles(elements) {
-        return nodeListToArray(elements).map(function (el) {
+    function getTiles(elements) {
+
+        return nodeListToArray(elements).map(function(el) {
             return {
                 "element": el,
                 "boundingRect": el.getBoundingClientRect()
             };
         });
     }
-    var els = document.querySelectorAll(selector);
-    console.log(els);
-    var organizedTiles = getOrganizedTiles(els);
 
-    //FIXME: find a better way to do this part:
-    var tileObjs = {};
-    var currentLine = 0;
+    function handleKeyPressTileMe(key) {
 
-    var yArray = getUniqueCoordinates(organizedTiles, "y");
-    var xArray = getUniqueCoordinates(organizedTiles, "x");
+        var rowDiff = 0;
+        var columnDiff = 0;
 
-    organizedTiles.forEach(function (tile) {
-
-        var y = tile.boundingRect.y;
-        var x = tile.boundingRect.x;
-
-        if (tileObjs[y] === undefined) {
-            tileObjs[y] = [];
+        if (key.keyCode === ARROW_UP) {
+            rowDiff = -1;
+        } else if (key.keyCode === ARROW_LEFT) {
+            columnDiff = -1;
+        } else if (key.keyCode === ARROW_RIGHT) {
+            columnDiff = 1;
+        } else if (key.keyCode === ARROW_DOWN) {
+            rowDiff = 1;
         }
 
-        tileObjs[y].push({
+        var oldTile = getTileGivenRowAndColumn(currentRow, currentColumn, tiles);
+        var newTile = getNextTile(currentRow, rowDiff, currentColumn, columnDiff, tiles);
+
+        changeClass(oldTile.tile.element, newTile.tile.element);
+        currentRow = newTile.row;
+        currentColumn = newTile.column;
+    }
+
+    function findPosition(axisPos, axisDiff, axisLength) {
+
+        var position = null;
+
+        (axisPos + axisDiff >= axisLength) ? position = 0: (axisPos + axisDiff < 0) ? position = axisLength - 1 : position = axisPos + axisDiff;
+
+        return position;
+    }
+
+    function getNextTile(currentRow, rowDiff, currentColumn, columnDiff, elements) {
+
+        var row = findPosition(currentRow, rowDiff, maxRowLength);
+        var column = findPosition(currentColumn, columnDiff, maxColumnLength);
+
+        return getTileGivenRowAndColumn(row, column, elements);
+    }
+
+    function getTileGivenRowAndColumn(row, column, elements) {
+
+        return elements.filter(function(element) {
+            return (element.row == row && element.column == column);
+        })[0];
+    }
+
+    function changeClass(oldElement, nextElement) {
+
+        oldElement.classList.remove('active');
+        nextElement.classList.add('active');
+    }
+
+    var _tiles = getTiles(document.querySelectorAll(selector));
+
+    //FIXME: find a better way to do this part:
+    var yArray = getUniqueCoordinates(_tiles, "y");
+    var xArray = getUniqueCoordinates(_tiles, "x");
+
+    _tiles.forEach(function(tile) {
+        var y = tile.boundingRect.y;
+        var x = tile.boundingRect.x;
+        tiles.push({
             "row": yArray.indexOf(y),
             "column": xArray.indexOf(x),
-            "element": tile
+            "tile": tile
         });
     });
-    console.log(tileObjs);
 
-
+    tiles[0].tile.element.classList.add("active");
+    document.addEventListener("keypress", handleKeyPressTileMe);
 })();
